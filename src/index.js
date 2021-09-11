44000f2b91c1f23502282e0f05c5d5450c3b1bb3
Rajat Kulkarni <rajatkulkarni95@gmail.com>
2021-09-11 19:31:22 +0530

    prisma import and basic query mutation

---
+++ src/index.js
@@ -0,0 +1,37 @@
+const { ApolloServer } = require("apollo-server");
+const fs = require("fs");
+const path = require("path");
+
+let links = [{
+    id: "link-0",
+    url: "www.howtographql.com",
+    description: "Fullstack tutorial for GraphQL",
+}, ];
+
+const resolvers = {
+    Query: {
+        info: () => "Hackernews API",
+        feed: () => links,
+    },
+    Mutation: {
+        post: (parent, args) => {
+            let idCount = links.length;
+
+            const link = {
+                id: `link-${idCount++}`,
+                description: args.description,
+                url: args.url,
+            };
+
+            links.push(link);
+            return link;
+        },
+    },
+};
+
+const server = new ApolloServer({
+    typeDefs: fs.readFileSync(path.join(__dirname, "schema.graphql"), "utf8"),
+    resolvers,
+});
+
+server.listen().then(({ url }) => console.log(`Server running at ${url}`));
